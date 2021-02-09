87449d5725d539d81522ab5b99d7f9c2ddf2f603
Jealine Gorre <54907417+whimsycode@users.noreply.github.com>
2021-02-09 22:35:17 +0800

    currently editing css

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!-- Footer -->
+
+<!DOCTYPE html>
+
+<html lang = "en">
+
+    <head>
+        
+        <meta charset="utf-8">
+
+        <!--Boostrap-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+        <!--Icon-->
+
+        <!-- <link rel = "icon" href = "title-icon.png"> -->
+
+        <!--Styles-->
+
+        <link href = "style/styles.css" rel = "stylesheet" type = "text/css">
+
+
+    </head>
+
+
+    <body>
+
+    </body>
+
+</html>
--- home.php
+++ home.php
@@ -1,55 +1,163 @@
+<!-- DevsLounge Home -->
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" href="style/devslounge.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"</script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-<?php include('navbar.php'); ?>
-<div class="container-fluid">
-  <img src="img/placeholderIMG.png" alt="Home Image" style="width:100%;">
-  <div class="centered"><h1>Placeholder Text</h1> above Placeholder Image</div>
-</div><br/>
-<h1 align="center">Featured Developers</h1>
+
+<html lang = "en">
+
+    <head>
+
+        <title>
+            DevsLounge 
+        </title>
+        
+        <meta charset="utf-8">
+
+        <!--Boostrap-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+        <!--Icon-->
+
+        <!-- <link rel = "icon" href = "title-icon.png"> -->
+
+        <!--Styles-->
+
+        <link href = "style/styles.css" rel = "stylesheet" type = "text/css">
 
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-		<div class="img-with-text">
-			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
-			<p>Developer 1</p>
-		</div>
-    </div>
-    <div class="col-sm">
-		<div class="img-with-text">
-			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
-			<p>Developer 2</p>
-		</div>
-    </div>
-    <div class="col-sm">
-		<div class="img-with-text">
-			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
-			<p>Developer 3</p>
-		</div>
-    </div>
-	<div class="col-sm">
-		<div class="img-with-text">
-			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
-			<p>Developer 4</p>
-		</div>
-    </div>
-	<div class="col-sm">
-		<div class="img-with-text">
-			<img src="img/placeholderDEV.jpg" alt="sometext" width="150px" height="300px" />
-			<p>Developer 5</p>
-		</div>
-    </div>
-  </div>
-</div>
+    </head>
 
-</body>
+
+
+	<body>
+
+		<!-- Navbar -->
+
+		<?php include('navbar.php'); ?>
+
+
+		
+		<!-- Home Contents -->
+
+		<!-- Jumbotron Image -->
+
+		<div class="container-fluid">
+
+			<img src="img/placeholderIMG.png" alt="Home Image" style="width:100%;">
+
+			<div class="centered">
+
+				<h1>Placeholder Text</h1> above Placeholder Image
+
+			</div>
+
+
+		</div>
+		
+		<br/>
+
+
+		<!-- Featured Developers -->
+		
+		<h1 align="center">Featured Developers</h1>
+		<br />
+
+
+		<div class = "container">
+
+			<div class = "row">
+			
+				<!--Developer 1-->
+				<div class="col-sm">
+					<div class="panel">
+
+						<div class=panel-heading>
+							<img src = "img/sample-dev.png" alt="Perez-dev" />
+							<b>Joshua Perez</b>
+							<br />
+							Software Developer 
+							<br />
+							Php300.00/hour 
+
+						</div>
+
+						<div class=panel-body>
+							Insert Skills here
+						</div>
+
+					</div>
+				</div>
+
+				<!--Developer 2-->
+				<div class="col-sm">
+					<div class="panel">
+
+						<div class=panel-heading>
+							<img src = "img/sample-dev.png" alt="Perez-dev" />
+							<b>Joshua Perez</b>
+							<br />
+							Software Developer 
+							<br />
+							Php300.00/hour 
+
+						</div>
+
+						<div class=panel-body>
+							Insert Skills here
+						</div>
+
+					</div>
+				</div>
+
+				<!--Developer 1-->
+				<div class="col-sm">
+					<div class="panel">
+
+						<div class=panel-heading>
+							<img src = "img/sample-dev.png" alt="Perez-dev" />
+							<b>Joshua Perez</b>
+							<br />
+							Software Developer 
+							<br />
+							Php300.00/hour 
+
+						</div>
+
+						<div class=panel-body>
+							Insert Skills here
+						</div>
+
+					</div>
+				</div>
+
+				<!--Developer 1-->
+				<div class="col-sm">
+					<div class="panel">
+
+						<div class=panel-heading>
+							<img src = "img/sample-dev.png" alt="Perez-dev" />
+							<b>Joshua Perez</b>
+							<br />
+							Software Developer 
+							<br />
+							Php300.00/hour 
+
+						</div>
+
+						<div class=panel-body>
+							Insert Skills here
+						</div>
+
+					</div>
+				</div>
+
+
+				
+
+			</div>
+		</div>
+
+	</body>
 </html>
