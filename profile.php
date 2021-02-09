5ad666732830636c6edb969c1d01b6ebbdedae75
johndaumentado <johndaumentado@gmail.com>
2021-02-09 10:13:26 +0800

    test commit

--- profile.php
+++ profile.php
@@ -1,23 +1,12 @@
 <!-- Dev Profile -->
-
-
 <!DOCTYPE html>
-
 <html lang = "en">
-
     <head>
-
         <title>
-            
             Profile | DevsLounge 
-        
         </title>
-
         <meta charset="utf-8">
-
-
         <!--Boostrap-->
-
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -33,8 +22,6 @@
 
 
     </head>
-
-
     <body>
 
         <!-- Navigation Bar -->
@@ -108,26 +95,9 @@
 
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-
-
-
                         </div>
-
                     </div>
-
-
                 </div>
-
-
-
-
-
             </div>  
-
-
-
-
-    
     </body>
-
 </html>
