dbc1b98507ea4721c6f9cf134c28db14b5a874b2
Cocobreads <cocobreading@gmail.com>
2021-02-09 12:28:51 +0800

    Second Commit

--- Login.php
+++ Login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="Logindesign.css">
+	<link rel="stylesheet" type="text/css" href="style/logindesign.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
