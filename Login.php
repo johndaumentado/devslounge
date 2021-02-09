6abe684d205a90669ff08fcfd133802992d96ff3
Cocobreads <cocobreading@gmail.com>
2021-02-09 16:27:40 +0800

    Commit

--- Login.php
+++ Login.php
@@ -3,33 +3,30 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="style/logindesign.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <div id="login">
+	<div id="login">
 		<form action="loginprocess.php" method="POST">
-				<h2> DevsLounge </h2>
+			<h2> DevsLounge </h2>
 			<p>
-					<label>Username:</label>
-					<input type="text" id="user" name="user" required="required" placeholder="Enter your Username..." />
+				<label>Username:</label>
+				<input type="text" id="user" name="user"  placeholder="Enter your Username..." />
 			</p>
-            <p>
-                     <label>Password:</label>
-					<input type="password" id="pass" name="pass" required="required" placeholder="Enter your Password..." />
-            </p>
-			    <br>
 			<p>
-                    <button type="submit"> Login </button>
-            </p>
-                <br>
-                <br>
-			        <a href="register.php">Click Here to Register!</a>
+				<label>Password:</label>
+				<input type="password" id="pass" name="pass"  placeholder="Enter your Password..." />
+			</p>
+			<br>
+			<p>
+				<button type="submit"> Login </button>
+			</p>
+			<a href="register.php">Click Here to Register!</a>
 		</form>
 	</div>
 </body>
-
 </html>
