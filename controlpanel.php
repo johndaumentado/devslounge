2cb3effe932b8d685ec82285e64487504e6ae5ff
johndaumentado <johndaumentado@gmail.com>
2021-02-10 21:45:41 +0800

    Updated navbar links.

--- controlpanel.php
+++ controlpanel.php
@@ -2,5 +2,7 @@
 <html lang="en">
 <head></head>
 <body>
+<h1>Currently under construction!</h1>
+<p><a href="home.php">Go back to home.</a></p>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,18 +1,13 @@
 <!-- Navbar -->
-
 <?php
 
     //session_start();
     //$_SESSION['type'] = "client";
 
 ?>
-
 <!DOCTYPE html>
-
 <html lang = "en">
-
     <head>
-
         <meta charset="utf-8">
 
         <!--Boostrap-->
@@ -21,32 +16,16 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-
         <!--Styles-->
-
         <link href = "style/nav-footer.css" rel = "stylesheet" type = "text/css">
-
-
-	
-
     </head>
-
-
-
-    <body >
-
+    <body>
     <!-- Navbar -->
     <nav class = "navbar navbar-fixed-top">
-
       	<!-- Logo Heading -->
-
 	 	<a class="navbar-brand" href="#">
-
     		<img src="img/devslounge-logo.png" width="210" height="60" alt="logo">
-
   		</a>
-		
-
 		<!-- Navbar Collapse -->
        	<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -54,48 +33,37 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
 
 			<!--Navbar Contents-->
-
 			<div class = "container-fluid">
-				
 				<ul class = "nav navbar-nav navbar-right">
-				
-
 					<!--PHP -->
 					<?php
-
 						// If logged in as admin 
 						if(isset($_SESSION['type'])&& $_SESSION['type']=="admin") {
 
-							echo  '<li class="nav-item"><a class="nav-link" href="#">Control Panel</a></li>
-								<li class="nav-item"><a class="nav-link" href="#">Job Postings</a></li>
+							echo  '<li class="nav-item"><a class="nav-link" href="controlpanel.php">Control Panel</a></li>
+								<li class="nav-item"><a class="nav-link" href="jobpostings.php">Job Postings</a></li>
 								<li class="nav-item"><a class="nav-link" href="#">Log-out</a></li>'; //add a button for this instead. 
 						}
 						
 						// If logged in as Developer or Client
 						else if(isset($_SESSION['type'])&& ( $_SESSION['type']=="developer" || $_SESSION['type']=="client" )) {
 
-							echo  '<li class="nav-item"><a class="nav-link" href="#">Job Postings</a></li>
+							echo  '<li class="nav-item"><a class="nav-link" href="jobpostings.php">Job Postings</a></li>
 									<li class="nav-item"><a class="nav-link" href="#">Profile</a></li>
 								<li class="nav-item"><a class="nav-link" href="#">Log Out</a></li>'; //add a button for this instead. 
 						}
-						
 						// User is not logged in (default navbar upon entering home page)
 						else {
 
 							echo  '<li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
-									<li class="nav-item"><a class="nav-link" href="#">Log In</a></li>
-									<li class="nav-item"><a class="nav-link" href="#">Register</a></li>';
+									<li class="nav-item"><a class="nav-link" href="login.php">Log In</a></li>
+									<li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>';
 						}
 					?>
 					
 				</ul>
-				
 			</div>
-
-
-
-    </nav>
-
+        </nav>
     </body>
 </html>
   
