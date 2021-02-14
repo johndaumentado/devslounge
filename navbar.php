ac2d9f4478b9cc0dd6d77b5579b9dafd33935f3b
Jealine Gorre <54907417+whimsycode@users.noreply.github.com>
2021-02-14 15:29:55 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -121,11 +121,13 @@
 
 	
 		<!-- Navbar Collapse -->
-	
+
+		<!-- TO FIX
+
        	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
 
 			<!--Navbar Contents-->
 			<div class = "container-fluid">
